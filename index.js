4e52214e65f7de6f827b8a9aae6914faaf83a8db
vyle4378 <vyle4378@gmail.com>
2024-12-31 12:31:19 -0800

    modify style of chatbox even further (multiline messages, get rid of \n in input box, renderHTML)

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function sendMessage(inputElem) {
     console.log(inputElem.value)
-    inputElem.value = ''
 }
 
+function renderChatMessage(inputElem, chatHTML) {
+    const chatAreaElem = document.querySelector('.chat-area')
+    let chatMessagesHTML = chatAreaElem.innerHTML
+
+    if (inputElem === chatInputElem) {
+        //console.log('out')
+        chatMessagesHTML += `<div class="chat-messages chat-out"><p>${chatHTML}</p></div>`
+    } else {
+        //console.log('in')
+        chatMessagesHTML += `<div class="chat-messages chat-in"><p>${chatHTML}</p></div>`
+    }
+    
+    chatAreaElem.innerHTML = chatMessagesHTML
+    inputElem.value = ''
+}
 
 //problem-box
 const problemButtonElem = document.querySelector('.problem-box > .enter-button')
@@ -11,7 +25,12 @@ const problemInputElem = document.querySelector('.problem-box > .input-space')
 problemButtonElem.addEventListener("click", () => sendMessage(problemInputElem))
 problemInputElem.addEventListener("keydown", (event) => {
     if (event.key === 'Enter') {
+        if (event.shiftKey) {return}
+        event.preventDefault()
         sendMessage(problemInputElem)
+
+        //problemInputElem.value = problemInputElem.value.replace(/\n/g, "<br>")
+        problemInputElem.value = ''
     } 
 })
 
@@ -20,10 +39,19 @@ problemInputElem.addEventListener("keydown", (event) => {
 const chatButtonElem = document.querySelector('.chat-box > .enter-button')
 const chatInputElem = document.querySelector('.chat-box > .input-space')
 
-chatButtonElem.addEventListener("click", () => sendMessage(chatInputElem))
+chatButtonElem.addEventListener("click", () => {
+    sendMessage(chatInputElem);
+    const chatHTML = chatInputElem.value.replace(/\n/g, "<br>")
+    renderChatMessage(chatInputElem, chatHTML)
+})
 chatInputElem.addEventListener("keydown", (event) => {
     if (event.key === 'Enter') {
+        if (event.shiftKey) {return}
+        event.preventDefault() // prevents \n at the end when enter is clicked
         sendMessage(chatInputElem)
+
+        const chatHTML = chatInputElem.value.replace(/\n/g, "<br>")
+        renderChatMessage(chatInputElem, chatHTML)
     } 
 })
 
