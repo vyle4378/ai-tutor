b933568bae47161059ab75f64512970fa7cd50de
vyle4378 <vyle4378@gmail.com>
2024-12-31 19:41:33 -0800

    add more style to chatbox (bot's thinking message, auto-scrolling to the bottom

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-function sendMessage(inputElem) {
-    console.log(inputElem.value)
-}
+const chatAreaElem = document.querySelector('.chat-area')
+let chatMessagesHTML = chatAreaElem.innerHTML
+const chatBoxElem = document.querySelector('.gpttext-container')
 
-function renderChatMessage(inputElem, chatHTML) {
-    const chatAreaElem = document.querySelector('.chat-area')
-    let chatMessagesHTML = chatAreaElem.innerHTML
-
-    if (inputElem === chatInputElem) {
-        //console.log('out')
-        chatMessagesHTML += `<div class="chat-messages chat-out"><p>${chatHTML}</p></div>`
-    } else {
-        //console.log('in')
-        chatMessagesHTML += `<div class="chat-messages chat-in"><p>${chatHTML}</p></div>`
-    }
-    
-    chatAreaElem.innerHTML = chatMessagesHTML
-    inputElem.value = ''
-}
 
 //problem-box
 const problemButtonElem = document.querySelector('.problem-box > .enter-button')
 const problemInputElem = document.querySelector('.problem-box > .input-space')
 
-problemButtonElem.addEventListener("click", () => sendMessage(problemInputElem))
+problemButtonElem.addEventListener("click", () => {handleProb})
 problemInputElem.addEventListener("keydown", (event) => {
     if (event.key === 'Enter') {
         if (event.shiftKey) {return}
         event.preventDefault()
-        sendMessage(problemInputElem)
-
-        //problemInputElem.value = problemInputElem.value.replace(/\n/g, "<br>")
-        problemInputElem.value = ''
+        handleProb()
     } 
 })
 
@@ -39,21 +21,63 @@ problemInputElem.addEventListener("keydown", (event) => {
 const chatButtonElem = document.querySelector('.chat-box > .enter-button')
 const chatInputElem = document.querySelector('.chat-box > .input-space')
 
-chatButtonElem.addEventListener("click", () => {
-    sendMessage(chatInputElem);
-    const chatHTML = chatInputElem.value.replace(/\n/g, "<br>")
-    renderChatMessage(chatInputElem, chatHTML)
-})
+chatButtonElem.addEventListener("click", () => {handleChat})
 chatInputElem.addEventListener("keydown", (event) => {
     if (event.key === 'Enter') {
         if (event.shiftKey) {return}
         event.preventDefault() // prevents \n at the end when enter is clicked
-        sendMessage(chatInputElem)
-
-        const chatHTML = chatInputElem.value.replace(/\n/g, "<br>")
-        renderChatMessage(chatInputElem, chatHTML)
+        handleChat()
     } 
 })
+
+function handleProb() {
+    sendMessage(problemInputElem)
+    //problemInputElem.value = problemInputElem.value.replace(/\n/g, "<br>")
+    problemInputElem.value = ''
+    setTimeout( () => {
+        chatAreaElem.scrollTo({
+            left: 0, 
+            top: chatAreaElem.scrollHeight,
+            behavior: "smooth"
+        })
+        renderChatMessage('bot', 'Thinking...')
+    }, 700)  
+         
+}
+
+function handleChat() {
+    sendMessage(chatInputElem);
+    const chatHTML = chatInputElem.value.replace(/\n/g, "<br>")
+    renderChatMessage('user', chatHTML)
+    setTimeout( () => {
+        chatAreaElem.scrollTo({
+            left: 0, 
+            top: chatAreaElem.scrollHeight,
+            behavior: "smooth"
+        })
+        renderChatMessage('bot', 'Thinking...')
+    }, 700)
+    
+}
+
+
+function sendMessage(inputElem) {
+    console.log(inputElem.value)
+}
+
+function renderChatMessage(type, chatHTML) {
+    if (type === 'user') {
+        //console.log('user')
+        chatMessagesHTML += `<div class="chat-messages user-message"><p>${chatHTML}</p></div>`
+        chatInputElem.value = ''
+    } else if (type === 'bot') {
+        //console.log('bot')
+        chatMessagesHTML += `<div class="chat-messages bot-message"><p>${chatHTML}</p></div>`
+    }
+    
+    chatAreaElem.innerHTML = chatMessagesHTML
+}
+
 
 
 //send question
